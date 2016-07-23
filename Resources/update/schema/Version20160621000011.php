ea7e30147091b8de1c7f11ac6a7e07803df36ec0
Sandro Groganz <sandro@campaignchain.com>
2016-07-23 11:47:15 -0700

    CampaignChain/campaignchain#206 Include system update routines for schema and data

---
+++ Resources/update/schema/Version20160621000011.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * Copyright 2016 CampaignChain, Inc. <info@campaignchain.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160621000011 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE campaignchain_operation_twitter_status (id INT AUTO_INCREMENT NOT NULL, operation_id INT DEFAULT NULL, message LONGTEXT NOT NULL, idStr VARCHAR(255) DEFAULT NULL, url VARCHAR(255) DEFAULT NULL, twitterData LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', createdDate DATETIME NOT NULL, modifiedDate DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_58E8118644AC3583 (operation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE campaignchain_operation_twitter_status ADD CONSTRAINT FK_58E8118644AC3583 FOREIGN KEY (operation_id) REFERENCES campaignchain_operation (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE campaignchain_operation_twitter_status');
+    }
+}
