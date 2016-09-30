1fa03de6610471073d0d906626ba401e0356252f
Sandro Groganz <sandro@campaignchain.com>
2016-09-30 14:20:49 +0200

    CampaignChain/campaignchain#223 Deal with identical content in campaigns

--- Validator/UpdateStatus.php
+++ Validator/UpdateStatus.php
@@ -77,11 +77,21 @@ class UpdateStatus extends AbstractOperationValidator
      */
     public function isExecutableByLocation($content, \DateTime $startDate)
     {
+        $endDateInterval = clone $startDate;
+        $endDateInterval->modify('+'.$this->maxDuplicateInterval);
+
         /*
          * If message contains no links, find out whether it has been posted before.
          */
         if($this->mustValidate($content, $startDate)){
-            if($this->schedulerUtil->isDueNow($startDate)) {
+            /*
+             * Is the Activity supposed to be executed now or is it scheduled
+             * within the time where a duplicate status error could emerge?
+             */
+            if(
+                $this->schedulerUtil->isDueNow($startDate) ||
+                ($startDate > new \DateTime() && $startDate < $endDateInterval)
+            ) {
                 /** @var TwitterUser $locationTwitter */
                 $locationTwitter = $this->em
                     ->getRepository('CampaignChainLocationTwitterBundle:TwitterUser')
